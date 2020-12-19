19fb9a0466207c21f8532bc84d664719ba440f08
luka177 <lukapanio@gmail.com>
2020-12-19 17:15:11 +0200

    Initial halium changes

--- lineage.mk
+++ lineage.mk
@@ -14,7 +14,7 @@
 # limitations under the License.
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
+#$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
 
 # Inherit device configuration for Fire HD 8 2018
 $(call inherit-product, device/amazon/karnak/lineage_karnak.mk)
--- lineage_karnak.mk
+++ lineage_karnak.mk
@@ -12,10 +12,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
+#$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/halium.mk)
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
 $(call inherit-product, device/amazon/karnak/device.mk)
 
