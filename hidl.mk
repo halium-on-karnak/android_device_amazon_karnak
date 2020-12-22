770bdc0fd755f064e4be055d7b4acd4ec92a9b72
luka177 <lukapanio@gmail.com>
2020-12-22 19:46:38 +0200

    Eh it breakes build

--- hidl.mk
+++ hidl.mk
@@ -82,8 +82,8 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@4.0-impl \
-    android.hardware.audio.effect@4.0-impl \
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio.effect@2.0-impl \
     android.hardware.audio@2.0-service 
 
 # RenderScript HAL
